83806e38e5de36f6e266e0ba348d9f2ccad0132f
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-03-04 12:13:35 +0200

    fixing errors of testing

---
+++ Desktop/Blog-App/config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kazuba1c@localhost/blogger'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kazuba1@localhost/blogger'
+
+class DevConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kazuba1@localhost/blogger'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+
---
+++ Desktop/Blog-App/tests/test_blog.py
@@ -0,0 +1,27 @@
+from app.models import Comment,User,Blog
+from app import db
+import unittest
+
+class BlogTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username = 'jeanne', password = 'kazubajoanna', email = 'njoanc@gmail.com@gmail.com')
+        self.new_blog = Blog(id = 123, blog_title = 'Blog', blog_content = 'Blog content',likes = 0, dislikes = 0)
+
+    def tearDown(self):
+        User.query.delete()
+        Blog.query.delete()
+        Comment.query.delete()
+
+    def test_check_instance(self):
+        self.assertEquals(self.new_blog.blog_title,'Blog')
+        self.assertEquals(self.new_blog.blog_content,'Blog content')
+        # self.assertEquals(self.new_blog.category,"blogup")
+
+    def test_save_blog(self):
+        self.new_blog.save_blog()
+        self.assertTrue(len(Blog.query.all()) > 0)
+
+    def test_get_blog_by_id(self):
+        self.new_blog.save_blog()
+        blog = Blog.get_blog(123)
+        self.assertTrue(blog is not None)
