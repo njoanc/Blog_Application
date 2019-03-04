78b20bde75b80823b88fcb3be48287ad304fb654
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-03-04 16:38:39 +0200

    deployment to heroku

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
-# app = create_app('development')
 app = create_app('production')
 
 manager = Manager(app)
